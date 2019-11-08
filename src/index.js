78b39a93119b1142069040699a4ee53f706a55f4
bouchja1 <jan.bouchner@gmail.com>
2019-11-08 18:16:30 +0100

    supported IE9

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'),
+);
