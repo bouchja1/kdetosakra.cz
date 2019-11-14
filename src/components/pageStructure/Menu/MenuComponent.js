1bebc7ac706afd30099dea560c8c9eb0767f005f
bouchja1 <jan.bouchner@gmail.com>
2019-11-14 15:30:24 +0100

    happy smile logo

--- src/components/pageStructure/Menu/MenuComponent.js
+++ src/components/pageStructure/Menu/MenuComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from 'antd';
 import awesomeLogo from '../../../assets/images/kdetosakra.svg';
+import smilingLogo from '../../../assets/images/kdetosakraSmile.svg';
 
 const MenuComponent = () => {
     return (
@@ -14,7 +15,7 @@ const MenuComponent = () => {
                     </div>
                 </div>
                 <div className="menu-item">
-                    <img src={awesomeLogo} alt="logo" className="kdetosakra-logo" />
+                    <img id="kdetosakra-logo" src={smilingLogo} alt="logo" className="kdetosakra-logo" />
                 </div>
                 <div className="menu-item info">
                     <Icon type="info-circle" theme="filled" style={{ color: 'rgb(97, 95, 95)' }} />
