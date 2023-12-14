5d356d9e61f516a5f29b028e9cedaed290894d04
bouchja1 <jan.bouchner@gmail.com>
2023-12-14 20:55:54 +0100

    add GA4

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { Layout } from 'antd';
 import classNames from 'classnames';
 import React from 'react';
 import { isMobile } from 'react-device-detect';
-import ReactGA from 'react-ga4';
 import { Provider } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -23,15 +22,16 @@ import store, { persistor } from './redux/store';
 
 const { Footer } = Layout;
 
+/*
 const initializeReactGA = () => {
     ReactGA.initialize('UA-151784741-1');
     ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 };
+ */
 
 export const App = () => {
     // init google analytics
     if (process.env.NODE_ENV === 'production') {
-        initializeReactGA();
         if (process.env.REACT_APP_SENTRY_DNS) {
             Sentry.init({
                 dsn: process.env.REACT_APP_SENTRY_DNS,
